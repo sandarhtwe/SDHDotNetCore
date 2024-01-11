1cfc8f5340edc40bef1197d2ed578f0de19c04d3
Sandar Htwe <sandarhtwe.sdh16@gmail.com>
2024-01-11 21:28:00 +0630

    [Update] Check Balance in MVC App

--- SDHDotNetCore.RestApi/Models/BlogResponseModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SDHDotNetCore.RestApi.Models
-{
-    public class BlogResponseModel
-    {
-        public bool IsSuccess { get; set; }
-        public string Message { get; set; }
-        public BlogDataModel Data { get; set; }
-    }
-}
