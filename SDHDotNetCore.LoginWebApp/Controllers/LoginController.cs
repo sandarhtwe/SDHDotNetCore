19afe595ba43eadf233fe9281f99b0f8b11747d4
Sandar Htwe <sandarhtwe.sdh16@gmail.com>
2024-01-13 04:17:00 +0630

    [Complete] LoginWebApp Controller

---
+++ SDHDotNetCore.LoginWebApp/Controllers/LoginController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SDHDotNetCore.LoginWebApp.Models;
+
+namespace SDHDotNetCore.LoginWebApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            var str = HttpContext.Session.GetString("LoginData");
+            if (str is not null) return Redirect("/");
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(LoginViewModel requestModel)
+        {
+            // logic
+
+            HttpContext.Session.SetString("LoginData", JsonConvert.SerializeObject(requestModel));
+
+            return Redirect("/");
+        }
+    }
+}
