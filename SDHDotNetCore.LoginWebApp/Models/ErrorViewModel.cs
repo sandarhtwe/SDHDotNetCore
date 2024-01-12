7a0d588ce65acd553694f614dfeab60a8dab03cb
Sandar Htwe <sandarhtwe.sdh16@gmail.com>
2024-01-13 04:17:55 +0630

    [Complete] LoginWebApp Model

---
+++ SDHDotNetCore.LoginWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SDHDotNetCore.LoginWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ SDHDotNetCore.LoginWebApp/Models/LoginViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace SDHDotNetCore.LoginWebApp.Models
+{
+    public class LoginViewModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
