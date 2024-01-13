0d5b3c3387d04f59ce286eb3890a4de1579617b8
Sandar Htwe <sandarhtwe.sdh16@gmail.com>
2024-01-14 01:15:35 +0630

    [Create] AppDbContext in MinimalApi and JSON camelcase off

---
+++ SDHDotNetCore.MinimalApi/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SDHDotNetCore.MinimalApi.Models;
+
+namespace SDHDotNetCore.MinimalApi.EFDbContext
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<BlogDataModel> Blogs { get; set; }
+    }
+}
--- SDHDotNetCore.MinimalApi/Program.cs
+++ SDHDotNetCore.MinimalApi/Program.cs
@@ -1,9 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using SDHDotNetCore.MinimalApi.EFDbContext;
+using SDHDotNetCore.MinimalApi.Features.Blog;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//Json CamelCase off
+builder.Services.ConfigureHttpJsonOptions(opt =>
+{
+    opt.SerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+    opt.SerializerOptions.PropertyNamingPolicy = null;
+});
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
+},
+ServiceLifetime.Transient,
+ServiceLifetime.Transient);
 
 var app = builder.Build();
 
@@ -16,29 +35,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+app.AddBlogService();
 
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
