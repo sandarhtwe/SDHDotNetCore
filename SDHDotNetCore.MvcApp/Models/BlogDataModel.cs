32ee47bd870ad99e5387ded4cf28f630a23b971d
Sandar Htwe <sandarhtwe.sdh16@gmail.com>
2024-01-14 23:50:24 +0630

    [Complete] BlogRestClient Mvc CRUD

--- SDHDotNetCore.MvcApp/Models/BlogDataModel.cs
+++ SDHDotNetCore.MvcApp/Models/BlogDataModel.cs
@@ -14,14 +14,79 @@ namespace SDHDotNetCore.MvcApp.Models
         [Key]
         [Column("BlogId")]
         public int BlogId { get; set; }
+        public string BlogTitle { get; set; }
+        public string BlogAuthor { get; set; }
+        public string BlogContent { get; set; }
+    }
 
-        [Column("BlogTitle")]
-        public string? BlogTitle { get; set; }
+    public class BlogDataResponseModel
+    {
+        public PageSettingModel PageSetting { get; set; }
+        public List<BlogDataModel> Blogs { get; set; }
+    }
 
-        [Column("BlogAuthor")]
-        public string? BlogAuthor { get; set; }
+    public class BlogResponseModel
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+        public BlogDataModel Data { get; set; }
+    }
 
-        [Column("BlogContent")]
-        public string? BlogContent { get; set; }
+    public class BlogListResponseModel
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+        public List<BlogDataModel> Data { get; set; }
+    }
+
+    public class PageSettingModel
+    {
+        public PageSettingModel()
+        {
+        }
+
+        public PageSettingModel(int pageNo, int pageSize, int pageCount, string pageUrl)
+        {
+            PageNo = pageNo;
+            PageSize = pageSize;
+            PageCount = pageCount;
+            PageUrl = pageUrl;
+        }
+
+        public int PageNo { get; set; }
+        public int PageSize { get; set; }
+        public int PageCount { get; set; }
+        public string PageUrl { get; set; }
+    }
+
+    public class MessageModel
+    {
+        public MessageModel() { }
+        public MessageModel(bool isSuccess, string message)
+        {
+            IsSuccess = isSuccess;
+            Message = message;
+        }
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+    }
+    public class BlogListResponseModel1
+    {
+        public List<BlogDataModel> BlogList { get; set; }
+        public int PageNo { get; set; }
+        public int PageSize { get; set; }
+        public int PageCount { get; set; }
+        public int PageRowCount { get; set; }
     }
 }
+
+//public class ScatterResponseModel
+//{
+//    public List<ScatterModel> Data { get; set; }
+//}
+
+//public class ScatterModel
+//{
+//    public string name { get; set; }
+//    public float[][] data { get; set; }
+//}
