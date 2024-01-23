80abfe97c50d292c6bc87c7db2dac56320f567da
Sandar Htwe <sandarhtwe.sdh16@gmail.com>
2024-01-23 11:46:51 +0630

    [Complete] Add log4net text log in Console App

--- SDHDotNetCore.LoggingConsoleApp/Program.cs
+++ SDHDotNetCore.LoggingConsoleApp/Program.cs
@@ -1,39 +1,60 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using log4net;
+using log4net.Config;
 using Serilog;
 using Serilog.Sinks.MSSqlServer;
 using System.Reflection;
 
-Console.WriteLine("Hello, World!");
-
-//to get the project name
-string projectName = Assembly.GetEntryAssembly()?.GetName()?.Name;
-
-Log.Logger = new LoggerConfiguration()
-			.MinimumLevel.Debug()
-			.WriteTo.Console()
-			.WriteTo./*File($"logs/{projectName}.txt", rollingInterval: RollingInterval.Day)*/
-				MSSqlServer(
-					connectionString: "Server=DESKTOP-DDE6MVJ\\TESTINGSDH;Database=TestDb;User ID=sa;Password=Sdh@1234;TrustServerCertificate=True;",
-					sinkOptions: new MSSqlServerSinkOptions
-					{
-						TableName = "LogEvents",
-						AutoCreateSqlTable = true
-					})
-			.CreateLogger();
-
-Log.Information("Hello, world!");
-
-int a = 10, b = 0;
-try
+internal class Program
 {
-	Log.Debug("Dividing {A} by {B}", a, b);
-	Console.WriteLine(a / b);
-}
-catch (Exception ex)
-{
-	Log.Error(ex, "Something went wrong");
-}
-finally
-{
-	await Log.CloseAndFlushAsync();
+	private static readonly ILog log4netLogger = LogManager.GetLogger(typeof(Program));
+	private static void Main(string[] args)
+	{
+		Console.WriteLine("Hello, World!");
+
+		//to get the project name
+		string projectName = Assembly.GetEntryAssembly()?.GetName()?.Name;
+
+		//Log.Logger = new LoggerConfiguration()
+		//			.MinimumLevel.Debug()
+		//			.WriteTo.Console()
+		//			.WriteTo./*File($"logs/{projectName}.txt", rollingInterval: RollingInterval.Day)*/
+		//				MSSqlServer(
+		//					connectionString: "Server=DESKTOP-DDE6MVJ\\TESTINGSDH;Database=TestDb;User ID=sa;Password=Sdh@1234;TrustServerCertificate=True;",
+		//					sinkOptions: new MSSqlServerSinkOptions
+		//					{
+		//						TableName = "LogEvents",
+		//						AutoCreateSqlTable = true
+		//					})
+		//			.CreateLogger();
+
+		// Configure log4net
+		var log4netRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+		XmlConfigurator.Configure(log4netRepository, new FileInfo("log4net.config"));
+
+		//Log.Information("Hello, world!");
+
+		// Log using log4net
+		log4netLogger.Info("Hello, world! (log4net)");
+
+
+		int a = 10, b = 0;
+		try
+		{
+			Log.Debug("Dividing {A} by {B}", a, b);
+			Console.WriteLine(a / b);
+		}
+		catch (Exception ex)
+		{
+			Log.Error(ex, "Something went wrong");
+		}
+		finally
+		{
+			// Close and flush log4net
+			LogManager.Shutdown();
+
+			// Close and flush Serilog
+			Log.CloseAndFlush();
+		}
+	}
 }
