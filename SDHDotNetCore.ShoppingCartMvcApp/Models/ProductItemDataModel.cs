ff576d7e3c0536638e33f929db37c9eb275e7ec3
Sandar Htwe <sandarhtwe.sdh16@gmail.com>
2024-01-25 17:03:23 +0630

    [Create] Shopping Cart Project Setup

---
+++ SDHDotNetCore.ShoppingCartMvcApp/Models/ProductItemDataModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SDHDotNetCore.ShoppingCartMvcApp.Models
+{
+    [Table("Tbl_item")]
+    public class ProductItemDataModel
+    {
+        [Key]
+        public int ItemId { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
